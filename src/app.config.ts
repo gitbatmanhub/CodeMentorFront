27951d334939f60dbbef9e9f8d88472b752f1286
EdwinDev <ecro98.98@gmail.com>
2025-12-08 19:38:27 -0500

    Add interceptor and add token in all requests

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,15 +1,21 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+import { AuthService } from '@/auth/services/auth';
+import { AuthGuard} from '@/auth/guards/auth.guard';
+import { AuthInterceptor} from '@/auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
-        provideHttpClient(withFetch()),
+        provideHttpClient(withInterceptorsFromDi()),
+        AuthService,
+        AuthGuard,
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
         provideAnimationsAsync(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
     ]
